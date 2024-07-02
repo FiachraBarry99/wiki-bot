49859c30b262b89857c4d039f60f5525e75298f4
Fiachra Barry <fiachrabarry@gmail.com>
2024-07-02 21:54:46 +0100

    added chunking

--- ingestion.py
+++ ingestion.py
@@ -4,11 +4,15 @@ from langchain_openai import OpenAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
 import os
 
+from langchain_text_splitters import CharacterTextSplitter
+
 
 load_dotenv()
-print(os.environ["PINECONE_API_KEY"])
 
 docs = WikipediaLoader(query="Aran Island", load_max_docs=3).load()
+
+text_splitter = CharacterTextSplitter(chunk_size=5000, chunk_overlap=100)
+texts = text_splitter.split_documents(docs)
 
 if "OPENAI_API_KEY" not in os.environ:
     raise EnvironmentError(f"Environment variable OPENAI_API_KEY is not set")
@@ -18,6 +22,7 @@ if "INDEX_NAME" not in os.environ:
 
 embeddings = OpenAIEmbeddings(openai_api_type=os.environ.get("OPENAI_API_KEY"))
 PineconeVectorStore.from_documents(
-    docs, embeddings,
+    texts, 
+    embeddings,
     index_name=os.environ.get("INDEX_NAME")
     )
