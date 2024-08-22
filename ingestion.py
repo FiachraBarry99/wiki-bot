d6c9bb850898ec194e1a9a5a648d99eeb04fb0b4
Fiachra Barry <fiachrabarry@gmail.com>
2024-08-22 14:43:07 +0100

    made ingestion and bot creation into functions

--- ingestion.py
+++ ingestion.py
@@ -9,20 +9,21 @@ from langchain_text_splitters import CharacterTextSplitter
 
 load_dotenv()
 
-docs = WikipediaLoader(query="Aran Island", load_max_docs=3).load()
+def create_vecstore(query):
+    docs = WikipediaLoader(query=query, load_max_docs=3).load()
 
-text_splitter = CharacterTextSplitter(chunk_size=5000, chunk_overlap=100)
-texts = text_splitter.split_documents(docs)
+    text_splitter = CharacterTextSplitter(chunk_size=5000, chunk_overlap=100)
+    texts = text_splitter.split_documents(docs)
 
-if "OPENAI_API_KEY" not in os.environ:
-    raise EnvironmentError(f"Environment variable OPENAI_API_KEY is not set")
+    if "OPENAI_API_KEY" not in os.environ:
+        raise EnvironmentError(f"Environment variable OPENAI_API_KEY is not set")
 
-if "INDEX_NAME" not in os.environ:
-    raise EnvironmentError(f"Environment variable INDEX_NAME is not set")
+    if "INDEX_NAME" not in os.environ:
+        raise EnvironmentError(f"Environment variable INDEX_NAME is not set")
 
-embeddings = OpenAIEmbeddings(openai_api_type=os.environ.get("OPENAI_API_KEY"))
-PineconeVectorStore.from_documents(
-    texts, 
-    embeddings,
-    index_name=os.environ.get("INDEX_NAME")
-    )
+    embeddings = OpenAIEmbeddings(openai_api_type=os.environ.get("OPENAI_API_KEY"))
+    PineconeVectorStore.from_documents(
+        texts, 
+        embeddings,
+        index_name=os.environ.get("INDEX_NAME")
+        )
--- stateful_bot.py
+++ stateful_bot.py
@@ -11,83 +11,90 @@ from langchain_core.runnables.history import RunnableWithMessageHistory
 
 load_dotenv()
 
-# initialise llm
-llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
+def answer_prompt(prompt, session_id):
+    # initialise llm
+    llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
 
-# create retriever
-embeddings = OpenAIEmbeddings()
-vectorstore = PineconeVectorStore(
-        index_name=os.environ["INDEX_NAME"], embedding=embeddings
-)
-retriever = vectorstore.as_retriever()
+    # create retriever
+    embeddings = OpenAIEmbeddings()
+    vectorstore = PineconeVectorStore(
+            index_name=os.environ["INDEX_NAME"], embedding=embeddings
+    )
+    retriever = vectorstore.as_retriever()
 
-# allow retriever to be aware of history
-contextualize_q_system_prompt = (
-    "Given a chat history and the latest user question "
-    "which might reference context in the chat history, "
-    "formulate a standalone question which can be understood "
-    "without the chat history. Do NOT answer the question, just "
-    "reformulate it if needed and otherwise return it as is."
-)
+    # allow retriever to be aware of history
+    contextualize_q_system_prompt = (
+        "Given a chat history and the latest user question "
+        "which might reference context in the chat history, "
+        "formulate a standalone question which can be understood "
+        "without the chat history. Do NOT answer the question, just "
+        "reformulate it if needed and otherwise return it as is."
+    )
 
-contextualize_q_prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", contextualize_q_system_prompt),
-        MessagesPlaceholder("chat_history"),
-        ("human", "{input}"),
-    ]
-)
+    contextualize_q_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", contextualize_q_system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
 
-history_aware_retriever = create_history_aware_retriever(
-    llm, retriever, contextualize_q_prompt
-)
+    history_aware_retriever = create_history_aware_retriever(
+        llm, retriever, contextualize_q_prompt
+    )
 
-# answer question
-system_prompt = (
-    "You are an assistant for question-answering tasks. "
-    "Use the following pieces of retrieved context to answer "
-    "the question. If you cannot answer the question based on the "
-    "context, say that you don't know."
-    "\n\n"
-    "{context}"
-)
+    # answer question
+    system_prompt = (
+        "You are an assistant for question-answering tasks. "
+        "Use the following pieces of retrieved context to answer "
+        "the question. If you cannot answer the question based on the given "
+        "context, say that you don't know and the question is not relevant."
+        "\n\n"
+        "{context}"
+    )
 
-qa_prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", system_prompt),
-        MessagesPlaceholder("chat_history"),
-        ("human", "{input}"),
-    ]
-)
+    qa_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
 
-question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+    question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
 
-rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+    rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
 
-# manage chat history
-store = {}      # simplify this so history is passed in as a dict or list
+    # manage chat history
+    store = {}      # simplify this so history is passed in as a dict or list
 
-def get_session_history(session_id: str) -> BaseChatMessageHistory:
-    if session_id not in store:
-        store[session_id] = ChatMessageHistory()
-    return store[session_id]
+    def get_session_history(session_id: str) -> BaseChatMessageHistory:
+        if session_id not in store:
+            store[session_id] = ChatMessageHistory()
+        return store[session_id]
 
-conversational_rag_chain = RunnableWithMessageHistory(
-    rag_chain,
-    get_session_history,
-    input_messages_key="input",
-    history_messages_key="chat_history",
-    output_messages_key="answer",
-)
+    conversational_rag_chain = RunnableWithMessageHistory(
+        rag_chain,
+        get_session_history,
+        input_messages_key="input",
+        history_messages_key="chat_history",
+        output_messages_key="answer",
+    )
+
+    res = conversational_rag_chain.invoke(
+        {"input": prompt},
+        config={
+            "configurable": {"session_id": session_id}
+        },
+    )
+
+    return res
+
+    
 
 # example invokation
 if __name__ == "__main__":
-    INPUT = "How many islands are in the Bahamas Islands?"
-    res = conversational_rag_chain.invoke(
-        {"input": INPUT},
-        config={
-            "configurable": {"session_id": "abc123"}
-        },
-    )
+    INPUT = "How many islands are in the Aran Islands?"
+    res = answer_prompt(prompt=INPUT, session_id='abc123')
 
     print(res["answer"])
